9fc12b8d2384c7673065ec78825578c45cd1a54c
Asfand322 <asfandsheikh321@gmail.com>
2024-10-01 20:04:16 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// for in loop
+// it will call keys of object
+
+let obj = {
+    ali: 20,
+    sam: 35,
+    ahmed: 43,
+    nouman: 45
+}
+for(let a in obj){
+    console.log(a)
+}
+// result will be
+// ali
+// sam
+// ahmed
+// nouman
+
+// for of loop
+// it will call values of an object
+
+let arfa = {
+    haris: 23,
+    faiyx: 36,
+    amaan: 42,
+    wasiq: 56
+}
+for(let b of "amaan"){
+    console.log(b)
+}
+// result will be
+// a
+// m
+// a
+// a
+// n
